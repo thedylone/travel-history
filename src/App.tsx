551055909f4bb38b40dbaf98287f5edc63abed9d
dylan <thedylone@gmail.com>
2023-02-17 01:42:44 +0000

    map responsive and highlightable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { MapChart } from './components/mapChart';
 
 function App() {
--- src/components/mapChart.tsx
+++ src/components/mapChart.tsx
@@ -4,15 +4,42 @@ import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simp
 
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+const mapWidth = 800;
+const mapHeight = 600;
 
 export const MapChart: FC = () => {
   return (
-    <ComposableMap>
-      <ZoomableGroup center={[0, 0]}>
+    <ComposableMap
+      width={mapWidth}
+      height={mapHeight}
+      projectionConfig={{ scale: 160 }}
+    >
+      <ZoomableGroup
+        translateExtent={[
+          [0, 0],
+          [mapWidth, mapHeight]
+        ]}
+      >
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                style={{
+                  default: {
+                    fill: geo.properties.name === "Denmark" ? "#F53" : "#D6D6DA",
+                    outline: "none"
+                  },
+                  hover: {
+                    fill: "#F53",
+                    outline: "none"
+                  },
+                  pressed: {
+                    fill: "#E42",
+                    outline: "none"
+                  }
+                }} />
             ))
           }
         </Geographies>
