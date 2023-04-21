72e6e6a287e4f0542d3461e3e02048c1eae89af1
dylan <thedylone@gmail.com>
2023-04-22 00:35:23 +0100

    time of day

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -10,12 +10,52 @@ import {
     setSelectedData,
 } from "./data";
 
+const location: ILocationData = {
+    name: "test",
+    date: "test",
+    lat: 0,
+    lng: 0,
+    images: [],
+};
+const label = (data: Object) => {
+    const merge = { ...location, ...data };
+    return `<h2 style="line-height:0">
+    ${merge.name}
+    </h2>`;
+};
+const homeSvg = `<svg viewBox="0 0 36 36">
+<path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+<circle fill="black" cx="14" cy="14" r="10"></circle>
+<path fill="currentColor" d="M 14,7 l -8,8 h3 v4 a1 1 0 0 0 1 1 h2 v-4 h4 v4 h2 a1 1 0 0 0 1 -1 v-4 h3 Z">
+</svg>`;
+const schoolSvg = `<svg viewBox="0 0 36 36">
+<path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+<circle fill="black" cx="14" cy="14" r="10"></circle>
+<path fill="currentColor" d="M 14,7 l -8,5 l 8,5 l 7.2,-4.5 v5 h 0.8 v-5.5 Z"/>
+<path fill="currentColor" d="M 14,18 l -4,-2.5 v 2 l 4,2.5 l 4,-2.5 v -2 Z"/>
+</svg>`;
+const workSvg = `<svg viewBox="0 0 36 36">
+<path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+<circle fill="black" cx="14" cy="14" r="10"></circle>
+<path fill="currentColor" d="M14,7 h-2 a1,1,0,0,0,-1,1 v1 h-3 a1,1,0,0,0,-1,1 v2 l7,1 l7,-1 v-2 a1,1,0,0,0,-1,-1 h-8 v-0.5 a0.5,0.5,0,0,1,0.5,-0.5 h3 a0.5,0.5,0,0,1,0.5,0.5 v0.5 h1 v-1 a1,1,0,0,0,-1,-1 z"/>
+<path fill="currentColor" d="M14,14 l-7,-1 v5 a1,1,0,0,0,1,1 h12 a1,1,0,0,0,1,-1 v-5 z"/>
+</svg>`;
+const geometry = new THREE.SphereGeometry(0.3, 32, 32);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const globeDay = "//unpkg.com/three-globe/example/img/earth-blue-marble.jpg";
+// const globeDay = `${process.env.PUBLIC_URL}/images/world.png`
+const globeNight = "//unpkg.com/three-globe/example/img/earth-night.jpg";
+const background = "//unpkg.com/three-globe/example/img/night-sky.png";
+// const background = `${process.env.PUBLIC_URL}/images/stars.png`
+const bump = "//unpkg.com/three-globe/example/img/earth-topology.png";
+
 const Map: FC<{
     open: boolean;
     setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }> = (props) => {
     const initTime = Date.now();
     const [rotate, setRotate] = useState(true);
+    const [day, setDay] = useState(true);
     const globeEl = useRef<GlobeMethods>();
     useEffect(() => {
         if (globeEl.current) {
@@ -29,48 +69,13 @@ const Map: FC<{
         props.setOpen(true);
         setSelectedData(data);
     };
-    const location: ILocationData = {
-        name: "test",
-        date: "test",
-        lat: 0,
-        lng: 0,
-        images: [],
-    };
-    const label = (data: Object) => {
-        const merge = { ...location, ...data };
-        return `<h2 style="line-height:0">
-        ${merge.name}
-        </h2>`;
-    };
-    const homeSvg = `<svg viewBox="0 0 36 36">
-    <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
-    <circle fill="black" cx="14" cy="14" r="10"></circle>
-    <path fill="currentColor" d="M 14,7 l -8,8 h3 v4 a1 1 0 0 0 1 1 h2 v-4 h4 v4 h2 a1 1 0 0 0 1 -1 v-4 h3 Z">
-  </svg>`;
-    const schoolSvg = `<svg viewBox="0 0 36 36">
-    <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
-    <circle fill="black" cx="14" cy="14" r="10"></circle>
-    <path fill="currentColor" d="M 14,7 l -8,5 l 8,5 l 7.2,-4.5 v5 h 0.8 v-5.5 Z"/>
-    <path fill="currentColor" d="M 14,18 l -4,-2.5 v 2 l 4,2.5 l 4,-2.5 v -2 Z"/>
-  </svg>`;
-    const workSvg = `<svg viewBox="0 0 36 36">
-    <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
-    <circle fill="black" cx="14" cy="14" r="10"></circle>
-    <path fill="currentColor" d="M14,7 h-2 a1,1,0,0,0,-1,1 v1 h-3 a1,1,0,0,0,-1,1 v2 l7,1 l7,-1 v-2 a1,1,0,0,0,-1,-1 h-8 v-0.5 a0.5,0.5,0,0,1,0.5,-0.5 h3 a0.5,0.5,0,0,1,0.5,0.5 v0.5 h1 v-1 a1,1,0,0,0,-1,-1 z"/>
-    <path fill="currentColor" d="M14,14 l-7,-1 v5 a1,1,0,0,0,1,1 h12 a1,1,0,0,0,1,-1 v-5 z"/>
-  </svg>`;
-    const geometry = new THREE.SphereGeometry(0.3, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ color: "red" });
-    const sphere = new THREE.Mesh(geometry, material);
     return (
         <>
             <Globe
                 ref={globeEl}
-                globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-                // globeImageUrl={`${process.env.PUBLIC_URL}/images/world.png`}
-                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-                // backgroundImageUrl={`${process.env.PUBLIC_URL}/images/stars.png`}
+                globeImageUrl={day ? globeDay : globeNight}
+                bumpImageUrl={bump}
+                backgroundImageUrl={background}
                 // points
                 pointsData={locationsData}
                 pointLabel={label}
@@ -89,7 +94,7 @@ const Map: FC<{
                 objectsData={locationsData}
                 objectLabel={label}
                 objectAltitude={0.02}
-                objectThreeObject={sphere}
+                objectThreeObject={new THREE.Mesh(geometry, material)}
                 onObjectClick={onClick}
                 // arcs
                 arcsData={arcsData}
@@ -118,7 +123,12 @@ const Map: FC<{
                     return element;
                 }}
             />
-            <Settings rotate={rotate} setRotate={setRotate} />
+            <Settings
+                rotate={rotate}
+                setRotate={setRotate}
+                day={day}
+                setDay={setDay}
+            />
         </>
     );
 };
--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,22 +1,35 @@
 import React, { FC } from "react";
 import "./settings.css";
 
+const createOption = (
+    name: string,
+    value: boolean,
+    setValue: (value: boolean) => void
+) => {
+    return (
+        <div className="settings__option">
+            <label htmlFor={name}>{name}</label>
+            <input
+                className="settings__checkbox"
+                checked={value}
+                type="checkbox"
+                id={name}
+                onChange={(e) => setValue(e.target.checked)}
+            />
+        </div>
+    );
+};
+
 const Settings: FC<{
     rotate: boolean;
     setRotate: React.Dispatch<React.SetStateAction<boolean>>;
+    day: boolean;
+    setDay: React.Dispatch<React.SetStateAction<boolean>>;
 }> = (props) => {
     return (
         <div className="settings">
-            <div className="settings__option">
-                <label htmlFor="rotate">Rotate</label>
-                <input
-                    className="settings__checkbox"
-                    checked={props.rotate}
-                    type="checkbox"
-                    id="rotate"
-                    onChange={(e) => props.setRotate(e.target.checked)}
-                />
-            </div>
+            {createOption("Rotate", props.rotate, props.setRotate)}
+            {createOption("Day", props.day, props.setDay)}
         </div>
     );
 };
