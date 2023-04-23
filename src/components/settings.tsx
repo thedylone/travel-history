df3db2d9cdb0330760263e7b506e6c676356a3af
dylan <thedylone@gmail.com>
2023-04-23 21:01:12 +0100

    trips options

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import "./settings.css";
+import _tripsJson from "../data/trips.json";
 
 const createOption = (
     name: string,
@@ -7,7 +8,7 @@ const createOption = (
     setValue: (value: boolean) => void
 ) => {
     return (
-        <div className="settings__option">
+        <div className="settings__option" key={name}>
             <label htmlFor={name}>{name}</label>
             <input
                 className="settings__checkbox"
@@ -20,6 +21,14 @@ const createOption = (
     );
 };
 
+const createTripOption = () => {
+    const options: JSX.Element[] = [];
+    for (const trip in _tripsJson) {
+        options.push(createOption(trip, true, () => {}));
+    }
+    return options;
+};
+
 const Settings: FC<{
     rotate: boolean;
     setRotate: React.Dispatch<React.SetStateAction<boolean>>;
@@ -30,9 +39,14 @@ const Settings: FC<{
 }> = (props) => {
     return (
         <div className="settings">
+            <div className="settings__wrapper">
+            <h1>Globe</h1>
             {createOption("Rotate", props.rotate, props.setRotate)}
             {createOption("Day", props.day, props.setDay)}
             {createOption("High Resolution", props.res, props.setRes)}
+            <h1>Trips</h1>
+            {createTripOption()}
+            </div>
         </div>
     );
 };
